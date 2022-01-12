2d514f23c539ceb985e8596ac27ba0b722480f23
alkesh patil <alkesh@patil.com>
2022-01-12 15:49:08 +0530

    small

--- components/home/SnapThree.jsx
+++ components/home/SnapThree.jsx
@@ -13,8 +13,8 @@ function SnapThree() {
     <>
       <div className="h-28 w-full"></div>
 
-      <div className="snap-center  text-white justify-center sm:text-left text- p-10 sm:p-0   flex flex-col sm:flex-row h-screen  ">
-        <div className="sm:w-1/2 w-full   flex flex-col  justify-center  ">
+      <div className="snap-center  text-white justify-center sm:text-left text-  sm:p-0   flex flex-col sm:flex-row h-screen  ">
+        <div className="sm:w-1/2 w-full p-10  flex flex-col  justify-center  ">
           <div className=" ">
             <HeadOne text={"Make best out of your money"} />
             <p className="mb-4 text-gray-500 sm:text-1xl text-sm ">
@@ -41,8 +41,8 @@ function SnapThree() {
           </div>
         </div>
         <div className="sm:w-1/2 w-full   to-transparent from-[#2B2B2B] flex items-center justify-center  ">
-          <div className=" w-full   pt-20 sm:pt-1 transition ease-in-out duration-1000 ">
-            <Image src={image} width={1300} height={1500} className="border w-full" />
+          <div className=" w-full border   sm:pt-1 transition ease-in-out duration-1000 ">
+            <Image src={image} width={1300} height={1500} className="border w-full h-full" />
           </div>
         </div>
       </div>
